19bc743410f5982e95f595cfd1f8195048b884d2
Devil078 <dimix98@bk.ru>
2019-05-23 11:09:57 +0300

    hw04-bytecode done

--- hw04-bytecode/src/main/java/ru/otus/bytecode/done/WithLog.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.bytecode.done;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface WithLog {
-}
