8b07c7e7e902f807e6fdf9e1a8731b1deb9fe1c7
DevOL078 <33911390+devol078@users.noreply.github.com>
2019-07-06 21:21:34 +0300

    hw05-gc

---
+++ hw05-gc/src/main/java/ru/otus/gc/OutMemoryMain.java
@@ -0,0 +1,42 @@
+package ru.otus.gc;
+
+/*
+-Xms256m
+-Xmx256m
+-Xlog:gc=debug:file=./hw05-gc/logs/gc-%p-%t.log:tags,uptime,time,level:filecount=5,filesize=10m
+-XX:+HeapDumpOnOutOfMemoryError
+-XX:HeapDumpPath=./hw05-gc/logs/dump
+-XX:+UseG1GC
+-XX:+UseSerialGC
+-XX:+UseParallelGC
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OutMemoryMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        doExperiment();
+    }
+
+    private static void doExperiment() throws InterruptedException {
+        final int limit = Integer.MAX_VALUE;
+
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1; i < limit; ++i) {
+            list.add(Integer.valueOf(i));
+
+            if(i % 1000000 == 0) {
+                for(int j = 0; j < list.size() / 2; ++j) {
+                    list.set(j, null);
+                }
+            }
+
+            if(i % 50000 == 0) {
+                Thread.sleep(1000);
+            }
+        }
+    }
+
+}
---
+++ hw05-gc/src/main/java/ru/otus/gc/analysis/LogAnalysis.java
@@ -0,0 +1,76 @@
+package ru.otus.gc.analysis;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class LogAnalysis {
+
+    private static final String logsPath = "./hw05-gc/logs/";
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("================G1 analysis====================");
+           analyzeLog("gc1.log");
+            System.out.println("================Parallel analysis====================");
+           analyzeLog("parallel_collector.log");
+            System.out.println("================Serial analysis====================");
+           analyzeLog("serial_collector.log");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void analyzeLog(String fileName) throws IOException {
+        Stream<String> lines = Files.lines(Paths.get(logsPath + fileName));
+        List<Double> youngReport = new ArrayList<>();
+        List<Double> fullReport = new ArrayList<>();
+        List<Double> remarkReport = new ArrayList<>();
+        for(String line: lines.collect(Collectors.toList())) {
+            if(line.contains("Young")) {
+                youngReport.add(getTimeFromString(line));
+            } else if(line.contains("Full")) {
+                fullReport.add(getTimeFromString(line));
+            } else if(line.contains("Remark")) {
+                remarkReport.add(getTimeFromString(line));
+            }
+        }
+
+        System.out.println("Young: " + youngReport.size());
+        for(Double d: youngReport) {
+            System.out.print(d + " ");
+        }
+        System.out.println();
+        double midYoung = youngReport.stream().reduce((a, b) -> a + b).get() / youngReport.size();
+        System.out.println("Young middle value: " + midYoung);
+
+        System.out.println("Full: " + fullReport.size());
+        for(Double d: fullReport) {
+            System.out.print(d + " ");
+        }
+        System.out.println();
+        double midFull = fullReport.stream().reduce((a, b) -> a + b).get() / fullReport.size();
+        System.out.println("Full middle value: " + midFull);
+
+        System.out.println("Remark: " + remarkReport.size());
+        for(Double d: remarkReport) {
+            System.out.print(d + " ");
+        }
+        System.out.println();
+        if(!remarkReport.isEmpty()) {
+            double midRemark = remarkReport.stream().reduce((a, b) -> a + b).get() / remarkReport.size();
+            System.out.println("Remark middle value: " + midRemark);
+        }
+    }
+
+    private static double getTimeFromString(String str) {
+        String[] arr = str.split(" ");
+        String time = arr[arr.length - 1];
+        return Double.parseDouble(time.substring(0, time.length() - 2));
+    }
+
+}
