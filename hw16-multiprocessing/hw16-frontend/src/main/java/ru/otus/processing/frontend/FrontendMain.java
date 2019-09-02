e8b830eed9092d7e80b264b62642d1df30e32b1c
DevOL078 <33911390+devol078@users.noreply.github.com>
2019-09-02 23:11:42 +0300

    hw16-message-system Save progress in Frontend.

--- hw16-multiprocessing/hw16-frontend/src/main/java/ru/otus/processing/frontend/FrontendMain.java
+++ hw16-multiprocessing/hw16-frontend/src/main/java/ru/otus/processing/frontend/FrontendMain.java
@@ -2,13 +2,18 @@ package ru.otus.processing.frontend;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class FrontendMain {
 
     private static final Logger logger = LoggerFactory.getLogger(FrontendMain.class);
 
     public static void main(String[] args) {
         logger.info("Hello from Frontend");
+
+        SpringApplication.run(FrontendMain.class, args);
     }
 
 }
---
+++ hw16-multiprocessing/hw16-frontend/src/main/java/ru/otus/processing/frontend/controller/UserController.java
@@ -0,0 +1,8 @@
+package ru.otus.processing.frontend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/users")
+public class UserController {
+
+}
